9280e230853b7aa82c9ecd0cf12124c6bdfaafbe
JackTranSSIS <94460655+JackTranSSIS@users.noreply.github.com>
2022-02-11 09:48:38 +0700

    Update cp_task_5.py

--- LightControlSystem/cp_task_5.py
+++ LightControlSystem/cp_task_5.py
@@ -1,1 +1,11 @@
+import board
+from analogio import AnalogOut, AnalogIn
 
+light_sensor = AnalogIn(board.A0)
+led = AnalogOut(board.A1
+
+led.value = 60000
+
+
+while True:
+  print(light_sensor.value)
